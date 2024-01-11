bc9e3d058e8e1932f4eab644e04e8ceb3c4d3c53
Eric Casanova <kilbane1407@gmail.com>
2024-01-11 20:39:00 -0300

    implementado login github

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,16 +4,24 @@ import Input from "../../components/atoms/Input/Input";
 import TextButton from "../../components/molecules/TextButton/TextButton";
 import TextTypeEffect from "../../components/molecules/TextTypeEffect/TextTypeEffect";
 import Logo from "../../components/atoms/Logo/Logo";
-
 import { useLogin } from "../../hooks/useLogin";
+import { useNavigate } from "react-router-dom";
+import { LAST_NEWS } from "../../constants/routes";
 
 const Login = () => {
   const [input, setInput] = useState({
     user: "",
     password: "",
   });
-  const { loginGoogle, loginUserPass } = useLogin();
+  const { loginGoogle, loginUserPass, loginGithub } = useLogin();
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleLogin = async (loginMethod) => {
+    try {
+      await loginMethod();
+      navigate(LAST_NEWS);
+    } catch (error) {}
+  };
   const handleChange = (id, value) => {
     setInput((prev) => ({ ...prev, [id]: value }));
   };
@@ -40,15 +48,25 @@ const Login = () => {
           onChange={handleChange}
           value={input.password}
         />
-        <TextButton onClick={() => loginUserPass(input.user,input.password)}>Login</TextButton>
+        <TextButton onClick={() => loginUserPass(input.user, input.password)}>
+          Login
+        </TextButton>
         <div className={styles.loginSocialMedia}>
           <div className={styles.btn}>
-            <TextButton icon={"google"} color="success" onClick={loginGoogle}>
+            <TextButton
+              icon={"google"}
+              color="success"
+              onClick={() => handleLogin(loginGoogle)}
+            >
               Google
             </TextButton>
           </div>
           <div className={styles.btn}>
-            <TextButton icon={"github"} color="black">
+            <TextButton
+              icon={"github"}
+              color="black"
+              onClick={() => handleLogin(loginGithub)}
+            >
               Github
             </TextButton>
           </div>
